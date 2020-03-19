50d842bec2a8eb1435e66aa70405d66bdff21bf4
Qingnan Lai <7454812+laiqn@users.noreply.github.com>
2020-03-19 10:17:16 +0800

    Update onlineidp.sh

--- idp3config/onlineidp.sh
+++ idp3config/onlineidp.sh
@@ -1,4 +1,4 @@
-cp -f /root/inst/idp3config/metadata-providers.xml /opt/shibboleth-idp/conf/metadata-provider.xml
+cp -f /opt/inst/idp3config/metadata-providers.xml /opt/shibboleth-idp/conf/metadata-provider.xml
 wget -O /opt/shibboleth-idp/metadata/carsifed-metadata-pre.xml https://www.carsi.edu.cn/carsimetadata/carsifed-metadata.xml
 chown -R tomcat.tomcat /opt/shibboleth-idp
 systemctl restart httpd
